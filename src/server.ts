cebc95a844e7b22b6256054d71a964fab48ceda9
LuisSempre <luis.silva@viannasempre.com.br>
2023-02-17 00:22:20 -0300

    fev 17 00:22

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,25 @@
 import fastify from 'fastify'
 import crypto from 'node:crypto'
+import { env } from './env'
 import { knex } from './database'
 
 const app = fastify()
 
 app.get('/hi', async () => {
-  const file = await knex('files').insert({
-    id: crypto.randomUUID(),
-    title: 'Files test',
-    amount: 1000,
-  })
+  const file = await knex('files')
+    .insert({
+      id: crypto.randomUUID(),
+      title: 'Files test',
+      amount: 1000,
+    })
+    .returning('*')
 
   return file
 })
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('HI')
